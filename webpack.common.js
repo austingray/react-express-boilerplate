0630ade6b8a8ff83aa60f91b5ba22c5a91cb165d
Austin Gray <waustingray@gmail.com>
2017-11-05 17:00:36 +0000

    cleaned up build files and started sketching future plans

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,34 @@
+/* eslint-disable */
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// the relative path to the build dir
+const buildDirPath = './public/build/js';
 
 module.exports = {
   entry: path.join(__dirname, 'src/react/App/index.jsx'),
+  plugins: [
+    new CleanWebpackPlugin([buildDirPath]),
+  ],
   output: {
-    path: path.join(__dirname, 'public/build/js'),
+    path: path.resolve(__dirname, buildDirPath),
     filename: 'bundle.js',
+    publicPath: buildDirPath,
   },
   resolve: {
+    modules: [
+      'node_modules',
+    ],
     extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?$/,
         exclude: [
           path.resolve(__dirname, 'node_modules'),
         ],
---
+++ webpack.dev.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'cheap-module-eval-source-map',
+});
